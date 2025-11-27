b28064728e85a5b82673e1daee8715bd57edda53
Andrew Smith <meuzishun@gmail.com>
2025-11-27 13:29:44 -0500

    Add Longest Palindromic Substring solution

--- medium/README.md
+++ medium/README.md
@@ -3,3 +3,4 @@
 ## Order
 
 1. 3Sum - Leetcode 15 ([file](3sum.py), [video](https://youtu.be/jzZsG8n2R9A?si=U4VYCpjrEtHH_JGF))
+2. Longest Palindromic Substring - Python - Leetcode 5 ([file](longest_palindromic_substring.py), [video](https://youtu.be/XYQecbcd6_c?si=PxXALH-NHBvFxqNX))
---
+++ medium/longest_palindromic_substring.py
@@ -0,0 +1,48 @@
+# Neetcode's solution
+
+class Solution:
+    def longestPalindrome(self, s):
+        res = ""
+        resLen = 0
+        
+        for i in range(len(s)):
+            # odd length
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+                
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        
+        return res
+
+# My adaptation
+
+class Solution:
+    def longestPalindrome(self, s):
+        res = ""
+        resLen = 0
+        
+        for i in range(len(s)):
+            l = i
+            r = i if len(s) % 2 == 1 else i + 1
+            
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r + 1]
+                    resLen = r - l + 1
+                    
+                l -= 1
+                r += 1
+                
+        return res
